733abe87ff68f9b4766bc5196caf9eac43ea5e36
tigabelass <ahmadaldomaulana13@gmail.com>
2024-11-03 07:58:48 +0700

    new lagi

--- perpustakaan/settings.py
+++ perpustakaan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pmw64p$ygpp#*qyn5)&p5!@$e^s85e#-sd_7iue@(yz(+xrqyc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'libraymawut.vercel.app', '.vercel.app']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / 'static')
 ]
